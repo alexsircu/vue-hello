db767a6649aa3b8921f58d17a93cc58ba08500f0
Alex Sircu <alex.sricu@gmail.com>
2020-12-04 15:42:12 +0100

    Aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ var app = new Vue(
  {
  el: '#root',
  data: {
-   message: "Hello World, this is my first project with Vue :D"
+   message: "Hello World, this is my first project with Vue :D",
+   imageMessage: "My first img with Vue",
+   image: "https://miro.medium.com/max/10000/0*hPVHCGVgepguJt8I"
  }
  }
 );
