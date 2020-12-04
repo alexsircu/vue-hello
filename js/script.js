4c3f53a1cd5f3832e99ac2a66292620699f14107
Alex Sircu <alex.sricu@gmail.com>
2020-12-04 15:23:24 +0100

    Stampare messaggio dentro h1 con Vue

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Con Vue, stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data.
+
+var app = new Vue(
+ {
+ el: '#root',
+ data: {
+   message: "Hello World, this is my first project with Vue :D"
+ }
+ }
+);
